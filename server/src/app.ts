9b6ae46a81c4e217482a926caf0df0169d4d646a
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-06-15 23:54:48 +0200

    Update for error handler midllewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ const server = express()
 
 import cors, { CorsOptions } from "cors"
 import listEndpoints from "express-list-endpoints"
+import { notFoundErrorHandler, forbiddenHandler, catchAllErrorHandler } from './errorHandler'
 
 import mongoose from "mongoose"
 mongoose.set('returnOriginal', false)
@@ -25,6 +26,12 @@ server.use(express.json({limit:'9mb'}))
 
 server.use("/", todoRoutes)
 //console.table(listEndpoints(server))
+
+// ******** ERROR MIDDLEWARES ************
+
+server.use(forbiddenHandler)
+server.use(notFoundErrorHandler)
+server.use(catchAllErrorHandler)
 
 const PORT: string | number = process.env.PORT || 4000
 const uri: string = `mongodb://${process.env.MONGO_DB}/${process.env.MONGO_PROJECT}`
